4733f6d8cd1a036ebc6747a2108849337d37f4fe
Lite-os15 <shivamdaki68@gmail.com>
2023-04-07 23:46:51 +0530

    initial commit

---
+++ lib/resources/auth_methods.dart
@@ -0,0 +1,48 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class AuthMethod {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore =FirebaseFirestore.instance;
+
+    // get user details
+    // Future<model.User> getUserDetails() async {
+    // User currentUser = _auth.currentUser!;  //signup user
+   Future<String> signUpUser({
+    required String email,
+    required String password,
+    required String username,
+    required String bio,
+    required Uint8List file,
+
+
+  })async{
+    String res ="Some error Occured";
+    try{
+      if(email.isNotEmpty || password.isNotEmpty ||username.isNotEmpty ||bio.isNotEmpty){
+        //register user
+       UserCredential cred = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+       print(cred.user!.uid);
+
+        // add user to database
+          await _firestore.collection('users').doc(cred.user!.uid).set({
+
+          'username':username,
+          'uid':cred.user!.uid,
+          'email':email,
+          'bio':bio,
+          'followers':[],
+          'following':[],
+        });
+
+        res ="success";
+      }
+    }
+    catch(err){
+      res = err.toString();
+    }
+    return res;
+  }
+}
--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,4 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:instagram_clone/resources/auth_methods.dart';
+import 'package:instagram_clone/utils/utils.dart';
 // import 'package:flutter_svg/flutter_svg.dart';
 // import 'package:instagram_clone/utils/colour.dart';
 import 'package:instagram_clone/widgets/text_field_input.dart';
@@ -16,8 +21,8 @@ class _SignUpScreenState extends State<SignUpScreen> {
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _bioController = TextEditingController();
   final TextEditingController _usernameController = TextEditingController();
-
-  //bool _isLoading = false;
+  Uint8List? _image;
+  bool _isLoading = false;
 
   @override
   void dispose() {
@@ -27,6 +32,13 @@ class _SignUpScreenState extends State<SignUpScreen> {
     _passwordController.dispose();
     _bioController.dispose();
     _usernameController.dispose();
+  }
+
+   selectImage() async {
+      Uint8List im = await pickImage(ImageSource.gallery);
+      setState(() {
+        _image =im;
+      });
   }
 
   @override
@@ -46,15 +58,24 @@ class _SignUpScreenState extends State<SignUpScreen> {
               //Circular widget to show the profile
                Stack(
                  children: [
-                   CircleAvatar(
-                     radius: 64,
-                     backgroundImage: NetworkImage(
-                         'https://images.unsplash.com/photo-1680553362060-9ed111958ada?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
-                     ),
-                   ),
+                _image != null
+                 ? CircleAvatar(
+                   radius: 64,
+                   backgroundImage: MemoryImage(_image!),
+                   backgroundColor: Colors.red,
+                 )
+
+                        :const CircleAvatar(
+                         radius: 64,
+                          backgroundImage: NetworkImage(
+                                    'https://i.stack.imgur.com/l60Hf.png'),
+                                backgroundColor: Colors.red,
+                           ),
                    Positioned(bottom: -10,
                        left: 80,
-                       child: IconButton(onPressed: (){},icon:const Icon(Icons.add_a_photo) ,)
+                       child: IconButton(onPressed: (){},
+                         icon:const Icon(Icons.add_a_photo)
+                         ,)
                    ),
                  ],
                ),
@@ -97,7 +118,17 @@ class _SignUpScreenState extends State<SignUpScreen> {
               ),
               //button login
               InkWell(
-                onTap: () {},
+                onTap: ()  async {
+                  String res = await AuthMethod().signUpUser(
+                      email: _emailController.text,
+                      password: _passwordController.text,
+                      username: _usernameController.text,
+                      bio: _bioController.text,
+                    file: _image!,
+
+                  );
+                  print(res);
+                },
                 child: Container(
                   child: const Text('Sign up'),
                   width: double.infinity,
@@ -109,7 +140,8 @@ class _SignUpScreenState extends State<SignUpScreen> {
                         Radius.circular(15),
                       ),
                     ),
-                    color: Colors.blue,),
+                    color: Colors.blue,
+                  ),
                 ),
               ),
 
